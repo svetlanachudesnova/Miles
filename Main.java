4da89240fb53c9e7b96cf1fc11c73b7d2190d7b4
svetlana chudesnova <chudesnova@gmail.com>
2021-05-20 20:03:33 +0300

    Main.java added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 30000;
+        int rubPerMile = 20;
+        int miles = price / rubPerMile;
+        System.out.println(miles);
+    }
+}
